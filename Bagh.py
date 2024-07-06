62711dd23692299b8894693972fdbc1bc74951e1
RupaliPrusty <rupali21@iiserbpr.ac.in>
2024-07-06 10:28:46 +0530

    Update Bagh.py

--- Bagh.py
+++ Bagh.py
@@ -1,5 +1,5 @@
 """
-Bagh- Python library for quantum chemistry calculation 
+Bagh- electronic structure theory package 
 """
 
 __version__ = "0.1.0"
@@ -9,15 +9,3 @@ class InvalidKindError(Exception):
     """Raised if the kind is invalid."""
     pass
 
-
-def get_random_ingredients(kind=None):
-    """
-    Return a list of random ingredients as strings.
-
-    :param kind: Optional "kind" of ingredients.
-    :type kind: list[str] or None
-    :raise lumache.InvalidKindError: If the kind is invalid.
-    :return: The ingredients list.
-    :rtype: list[str]
-    """
-    return ["shells", "gorgonzola", "parsley"]
